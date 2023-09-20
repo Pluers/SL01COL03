8c3cb439c4d628c02935f298fe71fcd20510775b
Rick <S1204255@student.windesheim.nl>
2023-09-20 09:13:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,21 @@
 /**
  * Schrijf een functie waarbij de kleur van de pagina
  * bepaald wordt op basis van tijd
-*/
+ */
+
+function GetTime()
+{
+    $time = date('H');
+    $style = "";
+
+    if ($time >= 6 && $time <= 18) {
+        $style = $style . "light";
+    } else {
+        $style = $style . "dark";
+    }
+
+    echo $style;
+}
 
 /**
  * Schrijf een functie waarbij de waarde van een variable getoond wordt, hoeveel het
